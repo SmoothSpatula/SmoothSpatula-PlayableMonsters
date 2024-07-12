cc7b47ef27df0565e04c6ff627b6b641bd1f02fc
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-07-12 17:28:36 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+-- PlayableMonsters v1.0.0
+-- SmoothSpatula
+log.info("Successfully loaded ".._ENV["!guid"]..".")
+
 local survivors = ... or {}
 
 local is_init = false
@@ -26,9 +30,31 @@ function setup_skill(skill_ref, name, description,
     return skill_ref
 end
 
+function setup_empty_skill(skill_ref)
+    skill_ref.token_name = "Locked"
+    skill_ref.token_description = ""
+    skill_ref.sprite = gm.constants.sRobomandoSkills
+    skill_ref.subimage = 3
+    skill_ref.animation = nil
+    skill_ref.cooldown = 0
+    skill_ref.damage = 0
+    skill_ref.is_primary = false
+    skill_ref.required_stock = 10000
+    skill_ref.use_delay = 0
+    skill_ref.require_key_press = 0
+    skill_ref.does_change_activity_state = false
+
+    local skills = gm.variable_global_get("class_skill")
+
+    skill_ref.on_can_activate = skills[1][25]
+    skill_ref.on_activate = skills[1][26]
+
+    return skill_ref
+end
+
 function setup_survivor(namespace, identifier, name, description, end_quote,
                         loadout_sprite, portrait_sprite, portraitsmall_sprite, palette_sprite, 
-                        walk_sprite, idle_sprite, death_sprite,
+                        walk_sprite, idle_sprite, death_sprite, jump_sprite, jumpfall_sprite,
                         colour, cape_array)
     
     survivor_id = gm.survivor_create(namespace, identifier)
@@ -50,7 +76,7 @@ function setup_survivor(namespace, identifier, name, description, end_quote,
     survivor.sprite_loadout_palette = palette_sprite
     survivor.sprite_credits = walk_sprite
 
-    survivor.primary_color = colour -- for stats screen
+    survivor.primary_color = gm.make_colour_rgb(colour.r, colour.g, colour.b) -- for stats screen
 
     local vanilla_survivor = survivor_setup.Survivor(0)
     survivor.on_init = vanilla_survivor.on_init
@@ -69,7 +95,9 @@ function setup_survivor(namespace, identifier, name, description, end_quote,
         ["identifier"] = identifier, 
         ["idle_sprite"] = idle_sprite, 
         ["walk_sprite"] = walk_sprite, 
-        ["death_sprite"] = death_sprite 
+        ["death_sprite"] = death_sprite,
+        ["jump_sprite"] = jump_sprite,
+        ["jumpfall_sprite"] = jumpfall_sprite
     }
 
     return survivor, survivor_id
@@ -86,9 +114,9 @@ local function setup_sprites(self)
     print("achieved setup_sprites")
     self.sprite_idle        = survivors[self.class].idle_sprite
     self.sprite_walk        = survivors[self.class].walk_sprite
-    self.sprite_jump        = survivors[self.class].walk_sprite
-    self.sprite_jump_peak   = survivors[self.class].walk_sprite
-    self.sprite_fall        = survivors[self.class].walk_sprite
+    self.sprite_jump        = survivors[self.class].jump_sprite
+    self.sprite_jump_peak   = survivors[self.class].jump_sprite
+    self.sprite_fall        = survivors[self.class].jumpfall_sprite or survivors[self.class].jump_sprite
     self.sprite_climb       = survivors[self.class].idle_sprite
     self.sprite_death       = survivors[self.class].death_sprite
     self.sprite_decoy       = survivors[self.class].death_sprite
@@ -109,7 +137,8 @@ end
 gm.post_script_hook(gm.constants.stage_load_room, function(self, other, result, args)
     if not is_init then 
         is_init = true
-        include_survivor("ImpOverlord")
+        include_survivor("MacrobicPredator")
+        
     end
 end)
 
